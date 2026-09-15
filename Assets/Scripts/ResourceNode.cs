04ec85489b06e6d4d001e682313294216650ebf2
jaee <dw031251@gmail.com>
2026-09-15 21:22:30 +0900

    test

--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -22,6 +22,7 @@ public class ResourceNode : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //test
         
     }
 
